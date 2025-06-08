47f5324799c302d774d79c0c9a14680cdde1c21b
Nikita Kolmogorov <backmeupplz@gmail.com>
2025-06-08 11:51:29 -0700

    fix: improve layout and add details about $SIMP distribution and airdrop criteria

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,23 @@ export default function () {
       <p>
         ca: <code className="break-all">0x0</code>
       </p>
-      <div className="flex flex-row gap-2 flex-wrap">
+      <div className="flex flex-row gap-2 flex-wrap items-center justify-center">
         <button className="btn btn-primary">Dex</button>
         <button className="btn btn-primary">Farcaster</button>
         <button className="btn btn-primary">Exp</button>
       </div>
+      <p>
+        Only 69,420,000 $SIMP will ever exist. 3% to the team. 17% to the
+        airdrops. 10% contingency fund locked behind 2/5 multisig between
+        <code>borodutch.eth</code>, <code>vitalik.eth</code>,{' '}
+        <code>jessepollak.eth</code>, <code>dwr.eth</code>, and{' '}
+        <code>varunsrin.eth</code>.
+      </p>
+      <p>
+        Airdrop will be proportional to: $SIMP holding, number of Farcaster and
+        X posts with "$SIMP" in them. Posts containing "What's $SIMP?" get 2x
+        the weight. $SIMP trading volume. And a few secret variables.
+      </p>
     </div>
   )
 }
